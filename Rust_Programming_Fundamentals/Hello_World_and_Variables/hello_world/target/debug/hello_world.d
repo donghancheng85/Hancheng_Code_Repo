03f5952582a1f827d1d2bff0b16cea748aad8555
donghancheng85 <donghancheng85@gmail.com>
2024-06-04 21:30:01 -0400

    init Rust folder

---
+++ Rust_Programming_Fundamentals/Hello_World_and_Variables/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/hancheng/Coding_Exercise/MyCodingGitRepo/Hancheng_Code_Repo/Rust_Programming_Fundamentals/Hello_World_and_Variables/hello_world/target/debug/hello_world: /home/hancheng/Coding_Exercise/MyCodingGitRepo/Hancheng_Code_Repo/Rust_Programming_Fundamentals/Hello_World_and_Variables/hello_world/src/main.rs
