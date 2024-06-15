463d6dfb2f483d6ac932525636b9739ebcab05ae
donghancheng85 <donghancheng85@gmail.com>
2024-06-15 14:17:24 -0400

    learned how to use rustfmt, what is void in rust

--- Rust_Programming_Fundamentals/2_Anatomy_of_Rust/basic_syntax/src/main.rs
+++ Rust_Programming_Fundamentals/2_Anatomy_of_Rust/basic_syntax/src/main.rs
@@ -1,3 +1,14 @@
+/**
+ * In vscode, we can us control+shit+p and select "Format Document" to format the code
+ * It uses the "rustfmt," which installed with Rust, to format the document for us.
+ */
 fn main() {
-    println!("Hello, world!");
+    let name: &str = "Michael";
+    let x: () = println!("My name is {}", name); // unit type "()" is the same as "void" in C++
+    let this_year: i32 = 2019;
+    let birth_year: i32 = 1985;
+    let age: i32 = this_year - birth_year;
+    let y: () = println!("I turned {} in {}", age, this_year);
+    assert_eq!(x, y);
+    let _z: () = println!("Thanks for chatting with me!");
 }
